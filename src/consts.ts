cd01070ae97f340416a229c53f66958e7dd262fe
Guilherme Gules Moreira <guilhermegules@gmail.com>
2025-02-09 17:49:08 -0300

    feat: create theme handler

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Guilherme Gules Moreira";
+export const SITE_DESCRIPTION =
+  "Blog about software development, tech and life!";
+export const SITE_THEME_KEY = "theme";
+export const DARK_THEME = "dark";
+export const LIGHT_THEME = "light";
